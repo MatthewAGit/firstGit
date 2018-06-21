e057e61a4968bf13fb8374444c12ba10927bac3f
matthew <matthew_java@163.com>
2018-06-21 14:58:02 +0800

    提交

--- firstGit/src/com/matthew/first/FirstDemo.java
+++ firstGit/src/com/matthew/first/FirstDemo.java
@@ -6,6 +6,10 @@ public class FirstDemo {
 		// TODO Auto-generated method stub
 
 		System.out.println(01);
+		/*******************这里是修改的*********************/
+		 String a = "1";
+		 String b = "2";
+		 System.out.println(a+b);
 	}
 
 }
